56bea82543e33d77965f9cae6dec0556695a7e4f
Deseteral <deseteral@gmail.com>
2019-11-01 08:57:25 +0100

    Testing on CI with Linux

--- build/unix.sh
+++ build/unix.sh
@@ -2,13 +2,15 @@
 
 cd build
 
+EXIV2_BUILD_NAME=''
 if [ "$(uname)" == "Darwin" ]; then
-    curl -o exiv2.tar.gz https://www.exiv2.org/builds/exiv2-0.27.2-Darwin.tar.gz
+    EXIV2_BUILD_NAME="exiv2-0.27.2-Darwin"
 else
-    curl -o exiv2.tar.gz https://www.exiv2.org/builds/exiv2-0.27.2-Linux64.tar.gz
+    EXIV2_BUILD_NAME="exiv2-0.27.2-Linux64"
 fi
 
+curl -o exiv2.tar.gz "https://www.exiv2.org/builds/$EXIV2_BUILD_NAME.tar.gz"
 mkdir -p exiv2
 tar xzf exiv2.tar.gz -C exiv2
-mv exiv2/exiv2-0.27.2-Darwin/bin/exiv2json exiv2/exiv2json
-rm -rf exiv2/exiv2-0.27.2-Darwin exiv2.tar.gz
+mv "exiv2/$EXIV2_BUILD_NAME/bin/exiv2json" exiv2/exiv2json
+rm -rf "exiv2/$EXIV2_BUILD_NAME" exiv2.tar.gz
