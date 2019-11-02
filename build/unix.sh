f53617b91ccc70174de3ff363b86661ddf0d29ee
Deseteral <deseteral@gmail.com>
2019-11-02 23:33:38 +0100

    dylib name in configuration

--- build/unix.sh
+++ build/unix.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-cd build
+EXIV2_BUILD_NAME=""
+EXIV2_LIB_NAME=""
+EXIV2_LIB_DIST_NAME=""
 
-EXIV2_BUILD_NAME=''
 if [ "$(uname)" == "Darwin" ]; then
     EXIV2_BUILD_NAME="exiv2-0.27.2-Darwin"
+    EXIV2_LIB_NAME="libexiv2.0.27.2.dylib"
+    EXIV2_LIB_DIST_NAME="libexiv2.27.dylib"
 else
     EXIV2_BUILD_NAME="exiv2-0.27.2-Linux64"
+    EXIV2_LIB_NAME="libexiv2.so.0.27.2"
+    EXIV2_LIB_DIST_NAME="libexiv2.so.0.27.2"
 fi
 
+
+cd build
 curl -o exiv2.tar.gz "https://www.exiv2.org/builds/$EXIV2_BUILD_NAME.tar.gz"
 mkdir -p exiv2
 tar xzf exiv2.tar.gz -C exiv2
