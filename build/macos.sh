22e8eccbfee1c311733f5dc49fc3f3c61dac1b06
Deseteral <deseteral@gmail.com>
2019-11-05 17:59:27 +0100

    Renamed dist to release

--- build/macos.sh
+++ build/macos.sh
@@ -2,7 +2,7 @@
 
 EXIV2_BUILD_NAME="exiv2-0.27.2-Darwin"
 EXIV2_LIB_NAME="libexiv2.0.27.2.dylib"
-EXIV2_LIB_DIST_NAME="libexiv2.27.dylib"
+EXIV2_LIB_RELEASE_NAME="libexiv2.27.dylib"
 
 cd build
 curl -o exiv2.tar.gz "https://www.exiv2.org/builds/$EXIV2_BUILD_NAME.tar.gz"
@@ -10,7 +10,7 @@ mkdir -p exiv2
 tar xzf exiv2.tar.gz -C exiv2
 
 mv "exiv2/$EXIV2_BUILD_NAME/bin/exiv2json" exiv2/exiv2json
-mv "exiv2/$EXIV2_BUILD_NAME/lib/$EXIV2_LIB_NAME" "exiv2/$EXIV2_LIB_DIST_NAME"
+mv "exiv2/$EXIV2_BUILD_NAME/lib/$EXIV2_LIB_NAME" "exiv2/$EXIV2_LIB_RELEASE_NAME"
 
 rm -rf "exiv2/$EXIV2_BUILD_NAME" exiv2.tar.gz
 
---
+++ build/make_release_macos.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PKG_VERSION=$(awk -F ' = ' '$1 ~ /version/ { gsub(/[\"]/, "", $2); printf("%s",$2) }' Cargo.toml)
+RELEASE_NAME="pastelogue_${PKG_VERSION}_MacOS"
+RELEASE_PATH="release/${RELEASE_NAME}"
+
+mkdir -p $RELEASE_PATH
+
+cp ./target/release/pastelogue $RELEASE_PATH
+cp ./build/exiv2/* $RELEASE_PATH
+
+cd release
+tar -czvf "${RELEASE_NAME}.tar.gz" $RELEASE_NAME
+cd ..
+
+rm -rf $RELEASE_PATH
