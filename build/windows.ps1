0dd7d6a4f22d79302d8856d8130a791c831fedf6
Michał Ferchow <deseteral@gmail.com>
2019-11-05 16:51:32 +0100

    Windows exiv2 build script

---
+++ build/windows.ps1
@@ -0,0 +1,12 @@
+$EXIV2_BUILD_NAME = "exiv2-0.27.2-2017msvc64"
+
+Set-Location .\build
+Invoke-WebRequest "https://www.exiv2.org/builds/$EXIV2_BUILD_NAME.zip" -OutFile ".\exiv2.zip"
+New-Item -Path "." -Name "exiv2" -ItemType "directory" -Force
+Expand-Archive ".\exiv2.zip" -DestinationPath ".\exiv2"
+
+Copy-Item ".\exiv2\$EXIV2_BUILD_NAME\bin\exiv2json.exe" -Destination "exiv2\exiv2json"
+Copy-Item ".\exiv2\$EXIV2_BUILD_NAME\bin\exiv2.dll" -Destination "exiv2\exiv2.dll"
+
+Remove-Item -LiteralPath ".\exiv2\$EXIV2_BUILD_NAME" -Force -Recurse
+Remove-Item -LiteralPath ".\exiv2.zip" -Force -Recurse
