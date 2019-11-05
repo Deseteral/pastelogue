156f5c35b56fc6937ad15d54a584dd1c7f3e5837
Deseteral <deseteral@gmail.com>
2019-11-05 17:55:02 +0100

    Make Windows release script

--- build/windows.ps1
+++ build/windows.ps1
@@ -5,7 +5,7 @@ Invoke-WebRequest "https://www.exiv2.org/builds/$EXIV2_BUILD_NAME.zip" -OutFile 
 New-Item -Path "." -Name "exiv2" -ItemType "directory" -Force
 Expand-Archive ".\exiv2.zip" -DestinationPath ".\exiv2"
 
-Copy-Item ".\exiv2\$EXIV2_BUILD_NAME\bin\exiv2json.exe" -Destination "exiv2\exiv2json"
+Copy-Item ".\exiv2\$EXIV2_BUILD_NAME\bin\exiv2json.exe" -Destination "exiv2\exiv2json.exe"
 Copy-Item ".\exiv2\$EXIV2_BUILD_NAME\bin\exiv2.dll" -Destination "exiv2\exiv2.dll"
 
 Remove-Item -LiteralPath ".\exiv2\$EXIV2_BUILD_NAME" -Force -Recurse
