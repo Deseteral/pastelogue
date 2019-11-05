9a227ae5b14d4f357711011d9e9460184d63e1c4
Deseteral <deseteral@gmail.com>
2019-11-05 15:31:43 +0100

    Let's deal with linux later

---
+++ build/macos.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+EXIV2_BUILD_NAME="exiv2-0.27.2-Darwin"
+EXIV2_LIB_NAME="libexiv2.0.27.2.dylib"
+EXIV2_LIB_DIST_NAME="libexiv2.27.dylib"
+
+cd build
+curl -o exiv2.tar.gz "https://www.exiv2.org/builds/$EXIV2_BUILD_NAME.tar.gz"
+mkdir -p exiv2
+tar xzf exiv2.tar.gz -C exiv2
+
+mv "exiv2/$EXIV2_BUILD_NAME/bin/exiv2json" exiv2/exiv2json
+mv "exiv2/$EXIV2_BUILD_NAME/lib/$EXIV2_LIB_NAME" "exiv2/$EXIV2_LIB_DIST_NAME"
+
+rm -rf "exiv2/$EXIV2_BUILD_NAME" exiv2.tar.gz
