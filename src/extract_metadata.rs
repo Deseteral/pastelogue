bc54589007ab80be975b1ccf5268037d5b246e2f
Deseteral <deseteral@gmail.com>
2019-12-27 16:51:12 +0100

    Leave TODO in code about creating custom assertion for datetime equality test

--- src/extract_metadata.rs
+++ src/extract_metadata.rs
@@ -37,8 +37,8 @@ mod tests {
         let metadata = PhotoMetadata::from_file(&path).unwrap();
 
         // then
-        assert_eq!(metadata.datetime.year, 2019);
-        assert_eq!(metadata.datetime.month, 8);
+        assert_eq!(metadata.datetime.year, 2019); // TODO: Create custom assertion like this:
+        assert_eq!(metadata.datetime.month, 8);  //       `assert_datetime_eq!(metadata.datetime, 2019, 12, 10, 13, 30)`
         assert_eq!(metadata.datetime.day, 4);
 
         assert_eq!(metadata.datetime.hour, 15);
