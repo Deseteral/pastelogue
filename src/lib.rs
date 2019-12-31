d3fac9162c4d72ad0ec9069dcdff8ade87435f24
Deseteral <deseteral@gmail.com>
2019-12-31 13:23:06 +0100

    Make library build

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod date_time;
+mod check_file;
+mod extract_metadata;
+mod fs_operations;
+mod processing;
+mod scan_dir;
+mod exiv2;
+
+pub use processing::process_dir as process_dir;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::env;
 use std::path::PathBuf;
 
-mod date_time;
-mod check_file;
-mod extract_metadata;
-mod fs_operations;
-mod processing;
-mod scan_dir;
-mod exiv2;
+use pastelogue::process_dir;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let root_path = PathBuf::from(&args[1]);
 
-    processing::process_dir(&root_path);
+    process_dir(&root_path);
 }
