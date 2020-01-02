5464a59047dc3ae0dd9915bcc2366d6c2e3ed4b0
Deseteral <deseteral@gmail.com>
2020-01-02 19:55:12 +0100

    Request/Response with named params

--- src/api.rs
+++ src/api.rs
@@ -6,10 +6,10 @@ use serde_json::{Value};
 #[serde(tag = "action")]
 enum Request {
     #[serde(rename = "START_PROCESSING")]
-    StartProcessing { path: String },
+    StartProcessing { args: StartProcessingArgs },
 
     #[serde(rename = "READ_EXIF_DATA")]
-    ReadExifData { path: String },
+    ReadExifData { args: ReadExifDataArgs },
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -19,22 +19,43 @@ enum Response {
     ProcessingStarted,
 
     #[serde(rename = "PROCESSING_PROGRESS")]
-    ProcessingProgress { progress: u32, total: u32 },
+    ProcessingProgress { payload: ProcessingProgressPayload },
 
     #[serde(rename = "PROCESSING_FINISHED")]
     ProcessingFinished,
 
     #[serde(rename = "EXIF_DATA")]
-    ExifData { exif_data: Value },
+    ExifData { payload: ExifDataPayload },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct StartProcessingArgs {
+    path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ReadExifDataArgs {
+    path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProcessingProgressPayload {
+    progress: u32,
+    total: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExifDataPayload {
+    exif_data: Value,
 }
 
 pub fn process_from_json_string(input: &str) {
     let req: Request = serde_json::from_str(input).unwrap();
 
     match req {
-        Request::StartProcessing { path } => {
+        Request::StartProcessing { args } => {
             send_response(Response::ProcessingStarted);
-            pastelogue::process_dir(&PathBuf::from(&path));
+            pastelogue::process_dir(&PathBuf::from(&args.path));
             send_response(Response::ProcessingFinished);
         },
         Request::ReadExifData { .. } => {
