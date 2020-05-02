2a9fd1a15fad71482c83bbb6ee630422a849ed4a
Deseteral <deseteral@gmail.com>
2020-05-02 13:00:09 +0200

    Simplify test code

--- tests/server_it_test.rs
+++ tests/server_it_test.rs
@@ -9,25 +9,34 @@ mod integration {
             .stdout(Stdio::piped())
             .spawn()
             .unwrap();
-            // .output()
-            // .expect("failed to execute process");
-
-        let start_processing_json = r#"{"action": "START_PROCESSING", "args": { "path": "./resources/test" } }"#;
 
         // when
-        {
-            let child_stdin = child.stdin.as_mut().unwrap();
-            child_stdin.write(format!("{}\n", &start_processing_json).as_bytes()).unwrap();
-        }
-        let output = child.wait_with_output().unwrap();
-        let output_str = String::from_utf8(output.stdout).unwrap();
+        let start_processing_json = r#"{"action": "START_PROCESSING", "args": { "path": "./resources/test" } }"#;
+        write_line_to_process(start_processing_json, &mut child);
 
         // then
-        let output_lines: Vec<&str> = output_str.split("\n").filter(|s| !s.is_empty()).collect();
+        let output_lines = get_process_output_lines(child);
         assert_eq!(output_lines, [
             r#"{"id":"PROCESSING_STARTED"}"#,
             r#"{"id":"PROCESSING_PROGRESS","payload":{"progress":1,"total":1,"path":"./resources/test/IMG_20190804_152120.jpg"}}"#,
             r#"{"id":"PROCESSING_FINISHED"}"#,
         ]);
+    }
+
+    fn write_line_to_process(line: &str, process: &mut Child) {
+        let process_stdin = process.stdin.as_mut().unwrap();
+        process_stdin.write(format!("{}\n", line).as_bytes()).unwrap();
+    }
+
+    fn get_process_output_lines(process: Child) -> Vec<String> {
+        let output = process.wait_with_output().unwrap();
+        let output_str = String::from_utf8(output.stdout).unwrap();
+        let output_lines: Vec<String> = output_str
+            .split("\n")
+            .filter(|s| !s.is_empty())
+            .map(|s| s.to_owned())
+            .collect();
+
+        output_lines
     }
 }
