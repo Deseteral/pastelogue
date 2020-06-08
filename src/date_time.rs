9e872c7850e48c6dc8bdb5f15131df859f093967
Deseteral <deseteral@gmail.com>
2020-06-08 20:03:49 +0200

    ISO date mapper test

--- src/date_time.rs
+++ src/date_time.rs
@@ -11,3 +11,20 @@ pub fn datetime_to_iso_string(datetime: &DateTime) -> String {
         datetime.second,
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_map_datetime_to_iso_string() {
+        // given
+        let datetime = DateTime::from_ascii(b"2020:06:08 20:02:24").unwrap();
+
+        // when
+        let iso_string: String = datetime_to_iso_string(&datetime);
+
+        // then
+        assert_eq!(iso_string, "2020-06-08T20:02:24");
+    }
+}
